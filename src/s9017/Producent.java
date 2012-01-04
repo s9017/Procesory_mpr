8410cdb9788df41dd7548c2914ee913b7365c30c
ThinkPad <ThinkPad@T60>
2012-01-04 17:42:11 +0100

    first commit

---
+++ src/s9017/Producent.java
@@ -0,0 +1,5 @@
+package s9017;
+
+public class Producent {
+
+}
---
+++ src/s9017/Start.java
@@ -0,0 +1,13 @@
+package s9017;
+
+public class Start {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
